fedded3b7586edab4594684f64d9e43d7e023169
ddrieck <ddrieck@gmail.com>
2014-04-15 13:52:02 -0400

    Added initial API code.

--- server.js
+++ server.js
@@ -16,6 +16,67 @@
 		app.use(express.methodOverride());
 	});
 
+	//define model ========
+	var Todo = mongoose.model('Todo', {
+		text : String
+	})
+
 	//listen (start app with node server.js) ==================
 	app.listen(8080);
 	console.log("App listening to port 8080");
+
+// routes =====================================================
+
+	//api
+	//get all todos
+	app.get('/api/todos', function(req,res){
+
+		//use mongoose to get all todos in the database
+		Todo.find(function(err, todos){
+
+				//if there is an error retrieving, send errors.
+				if (err)
+					res.send(err)
+
+				res.json(todos) //return todos in JSON format
+		});
+	});
+
+	//create todo, information comes from AJAX request from Angular
+	app.post('/api/todos', function(req,res){
+
+		//create a todo, information comes from Angular AJAX
+		Todo.create({
+
+			text : req.body.text,
+			done : false
+
+		}, function (err, todo) {
+
+			if (err)
+				res.send(err);
+
+			Todo.find(function(err, todos){
+				if (err)
+					res.send(err)
+				res.json(todos);
+				});
+		});
+	});
+
+	//delete a todo
+	app.delete('/api/todos/:todos_id', function(req,res){
+		Todo.remove({
+			_id : req.params.todos_id
+		}, function(err, todo) {
+			if (err)
+				res.send(err);
+
+			//get and return all the todos after you create another
+			Todo.find(function(err, todos){
+				if (err)
+					res.send(err)
+				res.json(todos);
+			});
+		});
+	});	
